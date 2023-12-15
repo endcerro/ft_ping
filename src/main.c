dd8aabca9610a7a7520f443ca2cbdf0817cbf7be
Enzo Dal Cerro <edal--ce@student.42.fr>
2023-12-15 03:30:02 +0100

    on the way

--- src/main.c
+++ src/main.c
@@ -11,41 +11,41 @@ void handler(int code){
 
 int getAddressFromHostname(const char* hostname , struct addrinfo **results);
 
-char *processAddrinfo(struct addrinfo *address){
-
-  if (address->ai_family == AF_INET) 
-  {
-            struct sockaddr_in *psai = (struct sockaddr_in*)address->ai_addr;
-        char ip[INET_ADDRSTRLEN];
-        if (inet_ntop(address->ai_family, &(psai->sin_addr), ip, INET_ADDRSTRLEN) != NULL) {
-            printf("IP: %s\n", ip);
-        }
+char *processAddrinfo(struct addrinfo *address)
+{
+  if (address->ai_family == AF_INET) {
+    struct sockaddr_in *psai = (struct sockaddr_in*)address->ai_addr;
+    char ip[INET_ADDRSTRLEN];
+    if (inet_ntop(address->ai_family, &(psai->sin_addr), ip, INET_ADDRSTRLEN) != NULL)
+      printf("IP: %s\n", ip);
   }
-  else if (address->ai_family == AF_INET6) {
-        struct sockaddr_in6 *psai = (struct sockaddr_in6*)address->ai_addr;
-        char ip[INET6_ADDRSTRLEN];
-        if (inet_ntop(address->ai_family, &(psai->sin6_addr), ip, INET6_ADDRSTRLEN) != NULL) {
-            printf("IP: %s\n", ip);
-        }
-        }
+  else if (address->ai_family == AF_INET6) 
+  {
+    struct sockaddr_in6 *psai = (struct sockaddr_in6*)address->ai_addr;
+    char ip[INET6_ADDRSTRLEN];
+    if (inet_ntop(address->ai_family, &(psai->sin6_addr), ip, INET6_ADDRSTRLEN) != NULL)
+        printf("IP: %s\n", ip);
+  }
   return 0;
 }
 
 int main(int argc, char **argv){
 
+  if (argc != 2)
+  {
+    printf("usage : ft_ping example.com %d\n", argc);
+    return 1;
+  }
   signal(SIGABRT, handler);
   const char *ip_addr = "163.172.250.16";
   const char *hostname = "42.fr";
 
   struct addrinfo *result = 0;
 
-  getAddressFromHostname("google.com", &result);
-  processAddrinfo(result);
-
-  freeaddrinfo(result);
-  // getAddressFromHostname("43.azerty",&result);
-  // freeaddrinfo(result);
-
-
+  if (getAddressFromHostname(argv[1], &result) == 0)
+  {
+    processAddrinfo(result);
+    freeaddrinfo(result);
+  }
   return 0;
 }
